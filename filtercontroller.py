887ae0dcc94cc036f539ba13e1532d55c95a9e7f
2083417w <2083417w@student.gla.ac.uk>
2015-10-04 00:43:25 +0100

    calling update_all_relevancies after marked relevant or irrelevant

--- filtercontroller.py
+++ filtercontroller.py
@@ -15,13 +15,14 @@ def update_all_relevancies():
 
 def add_tweet(tweet):
     tf.tweet_to_relevance_table(tweet)
-    update_all_relevancies()
     tr = tweet_relevancy(tweet)
     ttte.checkTweets(tweet[3], tweet[0], tweet[2], tweet[1][1], tweet[1][0], tr, tweet[4])
     
 
 def mark_irrelevant(tweet):
     tf.update_relevancy(tweet, false)
+    update_all_relevancies()
 
 def mark_relevant(tweet):
     tf.update_relevancy(tweet, true)
+    update_all_relevancies()
