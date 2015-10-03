3d0efe8313c73420bf9a8ed58b9ed2ff6367d04c
2083417w <2083417w@student.gla.ac.uk>
2015-10-03 23:11:12 +0100

    Updated order of columns in tweet table

--- DatabaseCreationScript.py
+++ DatabaseCreationScript.py
@@ -5,9 +5,9 @@ data = sqlite3.connect('data.db')
 c=data.cursor()
 
 #Create table for tweet data
-c.execute('''CREATE TABLE Tweet (postcode text, body text, time datetime, latitude float, longitude float, event integer)''')
+c.execute('''CREATE TABLE Tweet (body text, postcode text, latitude float, longitude float, url text UNIQUE, event integer, time datetime, relevance float);''')
 
 #Create table for event data
-c.execute('''CREATE TABLE  Event (id integer, postcode text, event text, time datetime, occurances integer)''')
+c.execute('''CREATE TABLE  Event (id integer, postcode text, event text, time datetime, occurances integer);''')
 
 data.close()
