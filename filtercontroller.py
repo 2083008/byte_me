529347a7bd2185656903d08dd1b2c74682fc5986
Alex Smith <a.smithycchs@gmail.com>
2015-10-04 01:18:16 +0100

    debugging

--- filtercontroller.py
+++ filtercontroller.py
@@ -9,13 +9,13 @@ def update_all_relevancies():
     c.execute('''SELECT * FROM Tweet''')
     tweets = c.fetchall()
     for tweet in tweets:
-        tr = tweet_relevancy(tweet)
+        tr = tf.tweet_relevancy(tweet)
         c.execute('''UPDATE Tweet SET relevance = ''', tr, ''' WHERE url = ''', tweet[4], ''';''')
 
 
 def add_tweet(tweet):
     tf.tweet_to_relevance_table(tweet)
-    tr = tweet_relevancy(tweet)
+    tr = tf.tweet_relevancy(tweet)
     ttte.checkTweets(tweet[3], tweet[0], tweet[2], tweet[1][1], tweet[1][0], tr, tweet[4])
     
 
